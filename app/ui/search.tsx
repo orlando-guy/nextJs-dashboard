17911d7741d3f56f43e18994d8fef5bf35e485f7
Orlando <guichardorlando@gmail.com>
2023-11-15 10:40:39 +0100

    complete Search and Table integration

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -18,7 +18,6 @@ export default function Search({ placeholder }: Readonly<{ placeholder: string }
     }
     replace(`${pathname}?${params.toString()}`)
   }
-  /* TODO Keeping the URL and Input in sync */
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -28,6 +27,7 @@ export default function Search({ placeholder }: Readonly<{ placeholder: string }
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {handleSearch(e.target.value)}}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
