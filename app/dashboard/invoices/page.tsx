3a768836627bb6b1f381597ea65937175a61726c
Orlando <guichardorlando@gmail.com>
2023-11-15 11:17:56 +0100

    imple., Search & Pagination w\ URLParams Next.APIs

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,16 +5,18 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons"
 import { lusitana } from '@/app/ui/font'
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons"
 import { Suspense } from 'react'
+import { fetchInvoicesPages } from "@/app/lib/data"
 
-export default function InvoicePage({ searchParams }:
+export default async function InvoicePage({ searchParams }:
     Readonly<{
         searchParams?: {
             query?: string;
             page?: string;
         }
     }>) {
-        const query = searchParams?.query ?? ''
-        const currentPage = Number(searchParams?.page) || 1
+    const query = searchParams?.query ?? ''
+    const currentPage = Number(searchParams?.page) || 1
+    const totalPages = await fetchInvoicesPages(query)
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -24,11 +26,11 @@ export default function InvoicePage({ searchParams }:
                 <Search placeholder="Search invoices..." />
                 <CreateInvoice />
             </div>
-             <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
                 <Table query={query} currentPage={currentPage} />
             </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                {/* <Pagination totalPages={totalPages} /> */}
+                <Pagination totalPages={totalPages} />
             </div>
         </div>
     )
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,7 +10,7 @@ export default function Search({ placeholder }: Readonly<{ placeholder: string }
 
   const handleSearch = (term: string) => {
     const params = new URLSearchParams(searchParams)
-
+    params.set('page', '1')
     if (term) {
       params.set('query', term)
     } else {
