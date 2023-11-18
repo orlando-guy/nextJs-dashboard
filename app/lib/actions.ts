6d706fd46771c4ea51821e321d16cced980062c1
Orlando <guichardorlando@gmail.com>
2023-11-18 14:37:48 +0100

    end create invoice & add update invoice with A.S

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod'
 import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
 const FormSchema = z.object({
     id: z.string(),
@@ -11,6 +13,8 @@ const FormSchema = z.object({
     date: z.string()
 })
 const CreateInvoice = FormSchema.omit({ id: true, date: true })
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -27,5 +31,25 @@ export async function createInvoice(formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `
-    /* TODO 6. Revalidate and redirect */
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice (id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    })
+
+    const amountInCents = amount * 100
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${ customerId }, amount=${amountInCents}, status=${status}
+        WHERE id = ${id}
+    `
+
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
 }
