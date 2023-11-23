a221fe8f674f1f3a9a56215d04c36daa83abb41d
Orlando <guichardorlando@gmail.com>
2023-11-23 11:06:19 +0100

    feat(login): add authentication & protect routes.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { signIn } from '@/auth'
 
 export type State = {
     errors?: {
@@ -113,5 +114,19 @@ export async function deleteInvoice(id: string) {
         return {
             message: 'Database Error: Failed to delete invoice.'
         }
+    }
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData
+) {
+    try {
+        await signIn('credentials', Object.fromEntries(formData))
+    } catch (error) {
+        if ((error as Error).message.includes('CredentialSignin')) {
+            return 'CredentialsSignin'
+        }
+        throw error
     }
 }
